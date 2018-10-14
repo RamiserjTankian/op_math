87f1dcc79c6d8ecab81616b717fb327ec7c373c3
Ramiserj <milinchi_@hotmail.com>
2018-10-14 18:23:21 -0500

    Fix "figura.toLowerCase()"
    El servidor si logra hacer la distincion entre Mayusculas y Minusculas, en localhost esta regla no importa pero en los servidores de hostinger si.

--- js/operaciones.js
+++ js/operaciones.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   $(".tabla-figuras").on("click", ".btncalcular", function(){
 
     var figura = $(this).attr("figura");
-  
-    window.location = figura+".php";
+    
+    window.location = figura.toLowerCase()+".php";
           
   })
 });
