1fe1c6f3da9571677200e71dcd124f4bdc91d081
Ramiro Diaz <milinchi_@hotmail.com>
2019-01-15 05:09:11 -0600

    Inicial
    Creacion del sitio web.

--- README.md
+++
@@ -1,3 +0,0 @@
-# op_math
-Sitio para publicar definiciones y realizar cálculos matemáticos.
-Demo: matematicas.caliope.xyx
--- rombo.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL ^ E_NOTICE);
-
-require_once "conf/config.php";
-require_once "pages/op.php";
-require_once "pages/header.php";
-require_once "pages/menu.php";
-require_once "pages/rombo.php";
-require_once "pages/footer.php";
